6704f39ae20df2dfcb8bc1b326a97f9443a5fb90
Robert Youngman <rob_youngman@yahoo.com>
2015-10-06 14:07:33 +0100

    Commit Adding the Worksace in Version4

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,12 @@
 $(document).on('click', '#go-search', function() {
   findImagesOnGoogle({keywords: $('#search-term').val(), container: '#search-results'})
 });
+$(document).on('click', '#search-results img', function() {
+  var url = $(this).data('url');
+  $("#workspace img").remove();
+  var img = $("<img>").attr('src', url);
+  $("#workspace").append(img);
+});
+$(document).on('input', '#text', function() {
+  $("#caption").text($(this).val());
+});
